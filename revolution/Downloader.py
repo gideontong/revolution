753d195353bc2630f66eba7e676931d918306858
Gideon Tong <gideon@gideontong.com>
2020-01-05 12:52:58 -0800

    Created template for definitions

--- revolution/Downloader.py
+++ revolution/Downloader.py
@@ -1,3 +1,17 @@
 class Downloader:
-    def __init__(self):
-        print("The constructor ran!")
+    path = ""
+
+    def __init__(self, path):
+        print("You attempted to create a Downloader object, but this does nothing.")
+        path = path
+    
+    # Automatically detect and attempt to download Wii video game
+    # Download if direct link
+    def download(self, url):
+        print("Passed url", url)
+        print("Will download to path", path)
+
+    # Pass a list of URLs to the downloader to automatically download it
+    def multidownload(self, list):
+        print("Passed list", list)
+        print("Will download to path", path)
